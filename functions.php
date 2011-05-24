9a272caaa71430a79bbc34e20565ac494f88100a
alex chousmith <alex@ninthlink.com>
2011-05-24 00:25:42 +0000

    [BugzId:3407] formatting COMMENTS and comment form

--- functions.php
+++ functions.php
@@ -175,6 +175,50 @@ function progo_summary( $morelink, $limit = 150 ) {
 	echo wpautop($content);
 }
 endif;
+if ( ! function_exists( 'progo_comments' ) ):
+function progo_comments($comment, $args, $depth) {	
+	$GLOBALS['comment'] = $comment;
+	extract($args, EXTR_SKIP);
+	
+	if ( 'div' == $args['style'] ) {
+		$tag = 'div';
+		$add_below = 'comment';
+	} else {
+		$tag = 'li';
+		$add_below = 'div-comment';
+	}
+	?>
+	<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
+	<?php if ( 'div' != $args['style'] ) : ?>
+	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+	<?php endif; ?>
+	<div class="comment-meta"><div class="comment-author vcard">
+	<?php echo get_comment_author_link() ?>
+	</div>
+	<div class="meta"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+		<?php
+			/* translators: 1: date, 2: time */
+			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'&nbsp;&nbsp;','' );
+		?>
+	</div>
+    </div>
+	<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+	<?php if ($comment->comment_approved == '0') : ?>
+	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+	<br />
+	<?php endif; ?>
+	<?php comment_text() ?>
+	
+	<div class="reply">
+	<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+	</div>
+	<?php if ( 'div' != $args['style'] ) : ?>
+	</div>
+	<?php endif; ?>
+	<?php
+}
+endif;
+
 /********* Back-End Functions *********/
 if ( ! function_exists( 'progo_admin_menu_cleanup' ) ):
 /**
