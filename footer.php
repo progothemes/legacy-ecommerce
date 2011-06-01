57da2499565ec374043e376f4548f276c031aac8
alex chousmith <alex@ninthlink.com>
2011-06-01 00:00:07 +0000

    freaking out for nothing

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 	</div><!-- #page -->
 	<div id="ftr" class="container_12">
     <div class="grid_8">
-<?php $fmenu = wp_nav_menu( array( 'container' => 'false', 'theme_location' => 'ftrlnx', 'echo' => '0' ) );
+<?php $fmenu = wp_nav_menu( array( 'container' => false, 'theme_location' => 'ftrlnx', 'echo' => 0 ) );
 $fmenu = str_replace('</li>','&nbsp;&nbsp;|&nbsp;&nbsp;</li>',substr($fmenu,0,strrpos($fmenu,'</li>'))) . "</li>\n</ul>";
 echo $fmenu;
 echo '<br />';
