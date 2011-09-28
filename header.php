211433c5811acd8dd62b0f1f14a0c5c2fbcdee85
alex chousmith <alex@ninthlink.com>
2011-09-28 18:55:45 +0000

    added a little more code so in case wp_title returns NOTHING ( aka on homepage & WP SEO plugin is not installed), at least show SOMETHING (site name | description)

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title( '' ); ?></title>
+<title><?php if ( wp_title( '', false ) ) { wp_title( '' ); } else { echo get_bloginfo( 'name' ) .' | '. get_bloginfo( 'description' ); } ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
